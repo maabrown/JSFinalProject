7138d9b18f35436214c101444c54248656ab1e72
Mark Brown II <markbrownii@Marks-MacBook-Pro.local>
2016-05-10 11:04:50 -0400

    Created results and login views for app

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+$('#results-view').hide();
 
 $(function() {
 
@@ -5,65 +6,79 @@ $results = $('#results');
 $albumArt = $('#albumArt')
 
 
-$.ajax({
-	url: 'https://en.wikipedia.org/w/api.php?action=query&titles=SWV&format=json&prop=extracts&exintro=&indexpageids=&explaintext=&callback=?',
-	type: 'GET',
-	dataType: 'json',
-	success: function(data) {
-		console.log(data);
-		console.log(data.query.pageids)
-		var pageids = data.query.pageids[0];
-		console.log(jQuery.type(pageids));
-		var extract = data.query.pages[pageids].extract
-		$results.append(extract);
+function newpage() {
+	$('#login-view').hide();
+	$('#results-view').show();
+		$.ajax({
+			url: 'https://en.wikipedia.org/w/api.php?action=query&titles=SWV&format=json&prop=extracts&exintro=&indexpageids=&explaintext=&callback=?',
+			type: 'GET',
+			dataType: 'json',
+			success: function(data) {
+				console.log(data);
+				console.log(data.query.pageids)
+				var pageids = data.query.pageids[0];
+				console.log(jQuery.type(pageids));
+				var extract = data.query.pages[pageids].extract
+				$results.append(extract);
 
-	},
-	error: function(errorMessage) {
-		console.log('not working');
-	}
-})
+			},
+			error: function(errorMessage) {
+				console.log('not working');
+			}
+		})
 
-$.ajax({
-	
-      	// var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+		$.ajax({
+			
+		      	// var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
 
-      	url: 'https://api.spotify.com/v1/search?q=SWV&type=artist',
-      	method: 'get',
-      	success: function(response) {
-      		// var artists = response.artists.items;
-      		
-      		// artists.forEach(function(artistObject) {
-      		// 	artistList.append("<li>" + artistObject.name +"</li>")
-      		// })
-      		console.log(response);
-      		var artRep = response.artists.items;
-      		console.log(response.artists.items);
-      		for (var i = 0; i < artRep.length; i++) {
-      			console.log(artRep[i].images);
-      			var images = artRep[i].images;
-      			var id = artRep[0].id;
-      			for (var a=0; a < images.length; a++) {
-      				console.log(images[a].url)
+		      	url: 'https://api.spotify.com/v1/search?q=SWV&type=artist',
+		      	method: 'get',
+		      	success: function(response) {
+		      		// var artists = response.artists.items;
+		      		
+		      		// artists.forEach(function(artistObject) {
+		      		// 	artistList.append("<li>" + artistObject.name +"</li>")
+		      		// })
+		      		console.log(response);
+		      		var artRep = response.artists.items;
+		      		console.log(response.artists.items);
+		      		for (var i = 0; i < artRep.length; i++) {
+		      			console.log(artRep[i].images);
+		      			var images = artRep[i].images;
+		      			var id = artRep[0].id;
+		      			for (var a=0; a < images.length; a++) {
+		      				console.log(images[a].url)
 
-      			}
-      		}
-      	$.ajax({
-      		url: 'https://api.spotify.com/v1/artists/' + id + '/albums',
-      		method: 'get',
-      		success: function(response) {
-      			console.log(response);
-      			var artistObj = response.items
-      			for (var i = 0; i  < artistObj.length; i++) {
-      				console.log(artistObj[i].images[1].url)
-      				$albumArt.append("<img src='" +artistObj[i].images[1].url +"'>")
-      			}
-      		},
-      		error: function(errorMessage) {
-      			console.log('Spotify not working');
-      		}
-      	})
+		      			}
+		      		}
+		      	$.ajax({
+		      		url: 'https://api.spotify.com/v1/artists/' + id + '/albums',
+		      		method: 'get',
+		      		success: function(response) {
+		      			console.log(response);
+		      			var artistObj = response.items
+		      			for (var i = 0; i  < artistObj.length; i++) {
+		      				console.log(artistObj[i].images[1].url)
+		      				$albumArt.append("<img src='" +artistObj[i].images[1].url +"'>")
+		      			}
+		      		},
+		      		error: function(errorMessage) {
+		      			console.log('Spotify not working');
+		      		}
+		      	})
 
-      	}
-      })
+		      	}
+		      })
+
+		$.ajax({
+			url: 'https://api.instagram.com/v1/tags/SWV/media/recent?access_token=ACCESS-TOKEN',
+			method: 'get',
+			dataType: 'jsonp',
+			success: function(response) {
+				console.log(response);
+			}
+		})
+
+} // new page function ends
 
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # GA JavaScript Class Final Project
 
-My application takes user input and gets information from the Spotify, Wikipedia (MediaWiki to be exact), and Instagram APIs.
+My application takes user input and gets information from the Spotify, Wikipedia (MediaWiki to be exact), and Instagram APIs.g
